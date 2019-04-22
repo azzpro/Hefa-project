47f42e8ae63744655dc62ac0d796d2803968b886
m18676389907@163.com <m18776389907@163.com>
2019-04-22 16:48:35 +0800

    1

---
+++ hefa-order-parent/azz-order-service/src/main/java/com/hefa/order/platform/controller/PayOrderManagementController.java
@@ -0,0 +1,22 @@
+package com.hefa.order.platform.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author THINK
+ * 订单支付管理
+ */
+@RestController
+@RequestMapping("/hefa/api/pay/order")
+public class PayOrderManagementController {
+	
+	//logger
+	private static final Logger logger = LoggerFactory.getLogger(PayOrderManagementController.class);
+	
+	
+	//@RequestMapping("payOrderList")
+	
+}
