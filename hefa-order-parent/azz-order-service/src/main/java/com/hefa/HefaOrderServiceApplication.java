0ba454cef46414319274739302517ae9e6025696
THINK <THINK@SZLX-20180112JJ>
2019-05-06 18:09:17 +0800

    1

--- hefa-order-parent/azz-order-service/src/main/java/com/hefa/HefaOrderServiceApplication.java
+++ hefa-order-parent/azz-order-service/src/main/java/com/hefa/HefaOrderServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableDiscoveryClient
